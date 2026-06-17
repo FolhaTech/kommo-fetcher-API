713935351375219f3f1e125a6d7ac232aa6c8103
carloswps <carloswps>
2026-06-16 23:07:21 -0300

    Add Kommo module and service with initial API request methods

---
+++ src/kommo/kommo.modulo.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { KommoService } from "./kommo.service";
+
+@Module({
+    providers: [KommoService],
+    exports: [KommoService]
+})
+
+export class KommoModule {}
